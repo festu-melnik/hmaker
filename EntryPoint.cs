a0e176e377867657ab00512dcddf553c429d198a
akm <melnik.festu@yandex.ru>
2014-04-04 20:34:42 +1000

    Fixed references.

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.Data.SQLite;
-using System.Data.Common;
+﻿using System.Data.Common;
 
 namespace hmaker
 {
