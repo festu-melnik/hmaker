f1ecd182090737a1a58b3e579b4e432ae6fe736a
akm <melnik.festu@yandex.ru>
2014-04-04 17:12:39 +1000

    Added SQLite references.

---
+++ EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using System.Data.Common;
+
+namespace hmaker
+{
+    class EntryPoint
+    {
+        public static void Main()
+        {
+            DbProviderFactory fact = DbProviderFactories.GetFactory("System.Data.SQLite");
+            using (DbConnection cnn = fact.CreateConnection())
+            {
+                cnn.ConnectionString = "Data Source=test.db3";
+                cnn.Open();
+            }
+        }
+    }
+}
